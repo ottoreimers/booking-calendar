7914fb88ac167afe9fc3246affd4a78d41e08dc8
Otto Reimers <otto.reimers@frojd.se>
2025-04-02 10:28:42 +0200

    Remove auth permissions for now

--- booking/views.py
+++ booking/views.py
@@ -2,7 +2,7 @@ from django.http import request
 from django.utils import timezone
 from rest_framework import viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
 from .models import Booking
@@ -11,7 +11,6 @@ from .serializers import BookingSerializer
 
 class BookingViewSet(viewsets.ModelViewSet):
     serializer_class = BookingSerializer
-    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return Booking.objects.all()
